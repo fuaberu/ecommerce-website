52e422658339e575c3f50ebe43309d4fe750f61e
fuaberu <kevinfabel@gmail.com>
2021-09-15 20:11:03 -0300

    add sign up and sign in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage/Homepage';
-import Registration from './pages/Registration/Registration';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 import './default.scss';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
 			<div className="main">
 				<Switch>
 					<Route exact path="/" component={Homepage} />
-					<Route path="/sign-in" component={Registration} />
+					<Route path="/sign-in" component={SignIn} />
+					<Route path="/sign-up" component={SignUp} />
 				</Switch>
 			</div>
 			<Footer />
