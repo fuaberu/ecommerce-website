44b6285f49e090a59e2af8fa0c05a2ae667f873a
fuaberu <kevinfabel@gmail.com>
2021-09-17 16:09:11 -0300

    firebase integration

---
+++ src/firebase/firebaseUtility.js
@@ -0,0 +1,52 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyC2rA0_cfY3SZLTyi7EAdUs-EmPLiReA48',
+	authDomain: 'my-ecommerce-3ce47.firebaseapp.com',
+	projectId: 'my-ecommerce-3ce47',
+	storageBucket: 'my-ecommerce-3ce47.appspot.com',
+	messagingSenderId: '532861101684',
+	appId: '1:532861101684:web:896037147225749dd21321',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+
+// google sign In
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+// password sign In
+
+
+// users profiles
+export const handleUserProfile = async (usersAuth, additionalData) => {
+	if (!usersAuth) return;
+	const { uid } = usersAuth;
+
+	const userRef = db.doc(`users/${uid}`);
+	const snapshot = await userRef.get();
+
+	if (!snapshot.exists) {
+		const { displayName, email } = usersAuth;
+		const time = new Date();
+		try {
+			userRef.set({
+				displayName,
+				email,
+				time,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	return userRef;
+};
