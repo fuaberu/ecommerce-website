0b193e4ae9f3fb92a392b9302bffa33be2207509
fuaberu <kevinfabel@gmail.com>
2021-09-24 16:10:12 -0300

    delete and add to cart

--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -3,20 +3,39 @@ import { SignInBtn } from '../small components/SmallComponents';
 import './productList.scss';
 
 const ProductList = (props) => {
-	const { image, alt, displayName, price, quantity } = props;
+	const {
+		image,
+		alt,
+		displayName,
+		price,
+		quantity,
+		handleClick,
+		uid,
+		handleDelete,
+	} = props;
 	return (
 		<div className="list-item">
 			<img className="product-image" src={image} alt={alt} />
 			<div>
 				<div className="flex">
 					<h4>{displayName}</h4>
-					<SignInBtn icon="fas fa-trash" />
+					<button onClick={(e) => handleDelete(e.target.dataset.uid)}>
+						<i id="delete" data-uid={uid} className="fas fa-trash"></i>
+					</button>
 				</div>
 				<div className="flex">
-					<p>{`Qty: ${quantity}`}</p>
+					<div className="quantity-container">
+						<button onClick={(e) => handleClick(e)}>
+							<i id="-" data-uid={uid} className="fas fa-minus"></i>
+						</button>
+						<p>{`${quantity}`}</p>
+						<button onClick={(e) => handleClick(e)}>
+							<i id="+" data-uid={uid} className="fas fa-plus"></i>
+						</button>
+					</div>
 					<p>{`$${price}`}</p>
 				</div>
-            </div>
+			</div>
 		</div>
 	);
 };
--- src/pages/productDetails/ProductDetails.js
+++ src/pages/productDetails/ProductDetails.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { myCart } from '../../App';
 import { db } from '../../firebase/firebaseUtility';
 import './productDetails.scss';
 
@@ -7,23 +9,59 @@ const ProductDetails = () => {
 	const [state, setState] = useState([]);
 	const { productID } = useParams();
 
+	const setCart = useContext(myCart).set;
+	const cart = useContext(myCart).value;
+
+	const handleAddToCart = (uidKey) => {
+		const objKeys = Object.keys(cart);
+		if (objKeys.some((element) => element === uidKey)) {
+			setCart({
+				...cart,
+				[uidKey]: {
+					displayName: cart[uidKey].displayName,
+					alt: cart[uidKey].alt,
+					image: cart[uidKey].image,
+					price: cart[uidKey].price,
+					quantity: cart[uidKey].quantity + 1,
+					keyId: cart[uidKey].uid,
+				},
+			});
+		} else {
+			setCart({
+				...cart,
+				[uidKey]: {
+					displayName: state.displayName,
+					alt: state.alt,
+					image: state.image,
+					price: state.price,
+					quantity: 1,
+					keyId: state.uid,
+				},
+			});
+		}
+	};
+
 	const fetchData = async (uid) => {
 		const userRef = db.doc(`products/${uid}`);
 		const snapshot = await userRef.get();
 		const data = snapshot.data();
+		data.uid = uid;
 		setState(data);
 	};
 	useEffect(() => {
 		fetchData(productID);
 	}, [productID]);
+
 	return (
 		<main className="product-detail-container">
 			<h1>{state.displayName}</h1>
 			<img src={state.image} alt={state.alt} />
 			<div className="flex">
 				<p>{`$${state.price}`}</p>
-				<button>
-					add to cart <i className="fas fa-cart-plus"></i>
+				<button onClick={() => handleAddToCart(productID)}>
+					<Link to="/cart">
+						add to cart <i className="fas fa-cart-plus"></i>
+					</Link>
 				</button>
 			</div>
 			<h3>{state.descriptionHeadLine}</h3>
